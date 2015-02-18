da875a03ef77354917a79d2475a986aa9d19e02c
shanedlee <slee@saltstack.com>
2015-02-18 21:22:27 +0000

    Removed exit line used for testing

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -77,8 +77,6 @@ echo "Refreshing the Environment Variables..."
 
 Update-Environment
 
-exit
-
 cls
 echo "Microsoft Visual C++ Compiler for Python (this may take a while)"
 $url = "http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
